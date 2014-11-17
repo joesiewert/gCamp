567161d8a9ac42248d6e3a6fdc06f1fb06fd3594
joesiewert <joe.siewert@gmail.com>
2014-11-16 21:14:21 -0700

    Updated task and user tests

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -2,94 +2,99 @@ require 'rails_helper'
 
 feature "Tasks" do
 
+  before :each do
+    @test_date = Time.now + 7.days
+    @test_date_expect = @test_date.strftime("%m/%d/%Y")
+  end
+
   scenario "User creates a task" do
     visit root_path
     click_on "Tasks"
     expect(page).to have_no_content("Create task")
     click_on "Create Task"
     fill_in "Description", with: "Create task"
-    fill_in "Due date", with: "15/12/2014"
+    fill_in "Due date", with: @test_date
     click_on "Create Task"
     expect(page).to have_content("Task was successfully created.")
     expect(page).to have_content("Create task")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
     click_on "Back"
     click_on "All"
     expect(page).to have_content("Create task")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
   end
 
-  scenario "User creates a task without a description" do
+  scenario "User creates a task without a description and date in past" do
     visit root_path
     click_on "Tasks"
-    expect(page).to have_no_content("12/15/2014")
     click_on "Create Task"
-    fill_in "Due date", with: "15/12/2014"
+    fill_in "Due date", with: "01/11/2014"
     click_on "Create Task"
     expect(page).to have_content("New task")
     expect(page).to have_content("Description can't be blank")
+    expect(page).to have_content("Due date can't be before today")
     click_on "Tasks"
-    expect(page).to have_no_content("12/15/2014")
+    expect(page).to have_no_content("11/01/2014")
   end
 
   scenario "User views a task" do
     Task.create!(
       description: "View task",
-      due_date: "15/12/2014"
+      due_date: @test_date
     )
 
     visit root_path
     click_on "Tasks"
     click_on "All"
     expect(page).to have_content("View task")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
     click_on "Show"
     expect(page).to have_content("View task")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
   end
 
   scenario "User edits a task" do
     Task.create!(
       description: "Edit task",
-      due_date: "15/12/2014"
+      due_date: @test_date
     )
 
     visit root_path
     click_on "Tasks"
     click_on "All"
     expect(page).to have_content("Edit task")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
     click_on "Edit"
     fill_in "Description", with: "Edited task"
-    fill_in "Due date", with: "17/12/2014"
+    fill_in "Due date", with: @test_date + 1.day
     check "Complete"
     click_on "Update Task"
     expect(page).to have_content("Task was successfully updated.")
     expect(page).to have_content("Edited task")
     expect(page).to have_content("True")
-    expect(page).to have_content("12/17/2014")
+    expect(page).to have_content((@test_date + 1.day).strftime("%m/%d/%Y"))
     expect(page).to have_no_content("Edit task")
-    expect(page).to have_no_content("12/15/2014")
+    expect(page).to have_no_content(@test_date_expect)
     click_on "Back"
     click_on "All"
     expect(page).to have_content("Edited task")
     expect(page).to have_content("True")
-    expect(page).to have_content("12/17/2014")
+    expect(page).to have_content((@test_date + 1.day).strftime("%m/%d/%Y"))
     expect(page).to have_no_content("Edit task")
-    expect(page).to have_no_content("12/15/2014")
+    expect(page).to have_no_content(@test_date_expect)
   end
 
   scenario "User edits a task to have no description" do
     Task.create!(
       description: "Edit task",
-      due_date: "15/12/2014"
+      due_date: @test_date
     )
 
     visit root_path
     click_on "Tasks"
     click_on "All"
     expect(page).to have_content("Edit task")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
     click_on "Edit"
     fill_in "Description", with: ""
     click_on "Update Task"
@@ -103,7 +108,7 @@ feature "Tasks" do
   scenario "User deletes a task" do
     Task.create!(
       description: "Delete task",
-      due_date: "15/12/2014",
+      due_date: @test_date,
       complete: false
     )
 
@@ -111,12 +116,12 @@ feature "Tasks" do
     click_on "Tasks"
     expect(page).to have_content("Delete task")
     expect(page).to have_content("False")
-    expect(page).to have_content("12/15/2014")
+    expect(page).to have_content(@test_date_expect)
     click_on "Destroy"
     expect(page).to have_content("Task was successfully destroyed.")
     click_on "All"
     expect(page).to have_no_content("Delete task")
     expect(page).to have_no_content("False")
-    expect(page).to have_no_content("12/15/2014")
+    expect(page).to have_no_content(@test_date_expect)
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -19,6 +19,36 @@ feature "Users" do
     expect(page).to have_content("joe@user.com")
   end
 
+  scenario "User creates a user with invalid data" do
+    visit root_path
+    click_on "Users"
+    click_on "Create User"
+    click_on "Create User"
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+  end
+
+  scenario "User creates a user with duplicate email" do
+    User.create!(
+      first_name: "Joe",
+      last_name: "User",
+      email: "joe@user.com",
+      password: "1234$!",
+    )
+
+    visit root_path
+    click_on "Users"
+    click_on "Create User"
+    fill_in "Email", with: "joe@user.com"
+    click_on "Create User"
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content("Email has already been taken")
+    expect(page).to have_content("Password can't be blank")
+  end
+
   scenario "User views a user" do
     User.create!(
       first_name: "Joe",
@@ -59,6 +89,28 @@ feature "Users" do
     expect(page).to have_content("jojo@tester.com")
     expect(page).to have_no_content("Joe User")
     expect(page).to have_no_content("joe@user.com")
+  end
+
+  scenario "User edits a user to have invalid data" do
+    User.create!(
+      first_name: "Joe",
+      last_name: "User",
+      email: "joe@user.com",
+      password: "1234$!",
+    )
+
+    visit root_path
+    click_on "Users"
+    expect(page).to have_content("joe@user.com")
+    click_on "Joe User"
+    click_on "Edit"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: ""
+    click_on "Update User"
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content("Email can't be blank")
   end
 
   scenario "User deletes a user" do
