b28e2346a3d06ea63e2f6c58e892b6974a07ad99
joesiewert <joe.siewert@gmail.com>
2014-11-17 15:34:50 -0700

    Downcase and strip spaces on email for signin and users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
+
+  before_validation do |user|
+    user.email = user.email.downcase.strip
+  end
 end
