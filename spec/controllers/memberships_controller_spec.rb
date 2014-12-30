db9b4b430c3e1e4d481277fc47246e5880055738
joesiewert <joe.siewert@gmail.com>
2014-12-29 21:53:59 -0600

    Added admin membership tests

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -27,6 +27,14 @@ describe MembershipsController do
       expect(response.status).to eq(200)
     end
 
+    it 'is visible to admin' do
+      project = create_project
+      user = create_user(admin: true)
+      session[:user_id] = user.id
+      get :index, project_id: project.id
+      expect(response.status).to eq(200)
+    end
+
     it 'is visible to project member' do
       project = create_project
       user = create_user
@@ -64,6 +72,21 @@ describe MembershipsController do
       user1 = create_user
       user2 = create_user
       create_membership(project, user1, role: "Owner")
+      session[:user_id] = user1.id
+      post :create,
+      project_id: project.id,
+      membership: {
+        project: project,
+        user: user2,
+        role: "Member"
+      }
+      expect(response.status).to eq(200)
+    end
+
+    it 'creates a membership as admin' do
+      project = create_project
+      user1 = create_user(admin: true)
+      user2 = create_user
       session[:user_id] = user1.id
       post :create,
       project_id: project.id,
@@ -138,6 +161,22 @@ describe MembershipsController do
       expect(response.status).to eq(302)
     end
 
+    it 'updates a membership as admin' do
+      project = create_project
+      user = create_user(admin: true)
+      membership = create_membership(project, user, role: "Owner")
+      session[:user_id] = user.id
+      put :update,
+      project_id: project.id,
+      id: membership.id,
+      membership: {
+        project: project,
+        user: user,
+        role: "Member"
+      }
+      expect(response.status).to eq(302)
+    end
+
     it 'does not update a membership as project member' do
       project = create_project
       user = create_user
@@ -183,9 +222,21 @@ describe MembershipsController do
 
     it 'deletes a membership as project owner' do
       project = create_project
-      user = create_user
-      membership = create_membership(project, user, role: "Owner")
-      session[:user_id] = user.id
+      user1 = create_user
+      user2 = create_user
+      create_membership(project, user1, role: "Owner")
+      membership = create_membership(project, user2)
+      session[:user_id] = user1.id
+      delete :destroy, project_id: project.id, id: membership.id
+      expect(response.status).to eq(302)
+    end
+
+    it 'deletes a membership as admin' do
+      project = create_project
+      user1 = create_user(admin: true)
+      user2 = create_user
+      membership = create_membership(project, user2)
+      session[:user_id] = user1.id
       delete :destroy, project_id: project.id, id: membership.id
       expect(response.status).to eq(302)
     end
