3beb116c4e3446852ca1279f39a5045ded78fb03
joesiewert <joe.siewert@gmail.com>
2015-01-01 17:41:12 -0600

    Migration for tracker token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222232254) do
+ActiveRecord::Schema.define(version: 20150101000550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141222232254) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.string   "tracker_token"
   end
 
 end
